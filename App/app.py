3bdd173d79e10cd1dfeb24afe0d10b2c52315f68
futpythontrader <107201702+futpythontrader@users.noreply.github.com>
2022-12-09 15:24:59 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import base64
 
 st.title("Web App Football Data")
 
@@ -40,8 +41,13 @@ df = load_data(selected_league, selected_season)
 st.subheader("Dataframe: "+selected_league)
 st.dataframe(df)
 
- 
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="Base_de_Dados.csv">Download CSV File</a>'
+    return href
 
+st.markdown(filedownload(df), unsafe_allow_html=True)
 
 
 
